95087ac0beb216b9edc184e80e534ec7bb1bb840
Oleksandr Shmatko <oleksandr.shmatko@masterofcode.com>
2024-09-01 08:48:34 +0300

    Hidden section course

--- app/js/main.js
+++ app/js/main.js
@@ -2,5 +2,18 @@ import './countdown-timer.js';
 import {onLoad as onGalleryLoad} from './gallery.js';
 
 document.addEventListener('DOMContentLoaded', function() {
+  document.querySelectorAll('.dropdown').forEach(function(dropdown) {
+    dropdown.addEventListener('click', function() {
+      const content = this.querySelector('.dropdown-content');
+      const icon = this.querySelector('.icon');
+
+      content.classList.toggle('open');
+      icon.classList.toggle('open');
+      this.classList.toggle('open');
+
+      icon.textContent = content.classList.contains('open') ? '−' : '+';
+    });
+  });
+
   onGalleryLoad();
 });
