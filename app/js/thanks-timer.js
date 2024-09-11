d34e5fa4c642da371f33ce1b7cc4900eacda4fef
greyzerua <greyzerua@ukr.net>
2024-09-11 20:00:52 +0300

    comment

--- app/js/thanks-timer.js
+++ app/js/thanks-timer.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
             if (secondsLeft === 0) {
                 clearInterval(countdownInterval);
-                // window.location.href = "https://t.me/canvacourse_bot?start=668ea76fbbee1bf0ab02839c";
+                window.location.href = "https://t.me/canvacourse_bot?start=668ea76fbbee1bf0ab02839c";
             }
         }, 1000);
     }
