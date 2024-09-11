c4dc37e59ad474431542a5f9013a9f55f4b6e82d
greyzerua <greyzerua@ukr.net>
2024-09-11 19:36:59 +0300

    add new page thanks

---
+++ app/js/thanks-timer.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const timerElement = document.querySelector('.timer60sec');
+
+    if (timerElement) {
+        let secondsLeft = 60;
+
+        const countdownInterval = setInterval(function() {
+            secondsLeft--;
+            timerElement.textContent = secondsLeft;
+
+            if (secondsLeft === 0) {
+                clearInterval(countdownInterval);
+                window.location.href = "https://t.me/canvacourse_bot?start=668ea76fbbee1bf0ab02839c";
+            }
+        }, 1000);
+    }
+})
